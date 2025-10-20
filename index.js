00be992b19c7b3c60d684b28a36a65b6e201f034
Midnight Doggo <git@midnightdoggo19.com>
2025-10-20 23:19:23 +0100

    slash command

--- index.js
+++ index.js
@@ -4,23 +4,33 @@ const {
     GatewayIntentBits,
     Events,
     Partials,
-    EmbedBuilder
+    EmbedBuilder,
+    SlashCommandBuilder,
+    REST,
+    Routes
 } = require('discord.js');
 const crypto = require('crypto');
 
 const anonymousChannelID = process.env.ANONYMOUS_CHANNEL;
-const roleName = process.env.ROLE_NAME;
+// const roleName = process.env.ROLE_NAME;
 const modChannelID = process.env.MOD_CHANNEL;
 const token = process.env.DISCORD_TOKEN;
-let inputChannelID = process.env.INPUT_CHANNEL; // cuz it can change
+// let inputChannelID = process.env.INPUT_CHANNEL; // cuz it can change
 const avatarToken = process.env.AVATAR_TOKEN || Math.random();
 
-if (!anonymousChannelID || !token) {
+const rest = new REST().setToken(token);
+
+if (
+    !anonymousChannelID ||
+    !token ||
+    !process.env.ID ||
+    !process.env.GUILDID
+) {
     console.log('Please set all required environment variables!');
     process.exit(1);
-} if (!inputChannelID) {
-    inputChannelID = anonymousChannelID // down here
-}
+} // if (!inputChannelID) {
+    // inputChannelID = anonymousChannelID // down here
+// }
 
 let client = new Client({
     intents: [
@@ -34,7 +44,12 @@ let client = new Client({
         Partials.Channel,
         Partials.Message
     ] // for DM permissions
-})
+});
+
+const commandSend = new SlashCommandBuilder()
+	.setName('send')
+	.setDescription('Anonymously send a message')
+	.addStringOption((option) => option.setName('message').setDescription('The message to send'));
 
 const emojis = [
     '🐶', '🐱', '🐭', '🐹', '🐰', '🦊', '🐻', '🐼', '🐨', '🐯',
@@ -45,10 +60,20 @@ const emojis = [
     '🦈', '🐋', '🐳', '🐬', '🐡', '🌿', '🌱', '🌳', '🌴', '🌵',
     '🌷', '🌹', '🌺', '🌻', '🌼', '🌸', '🌾', '🍀', '🍁', '🍄',
     '🌰', '🌲', '🐊', '🦢', '⚡️', '🔥', '💧', '🐟', '🐠', '🦌',
-    '🐽', '🌪️',  '🌊', '🌬️',  '☀️',  '⛅', '🌈', '⚡', '❄️',  '💨',
-    '🌁', '🌤️',  '🌥️',  '🌦️',  '🌧️',  '⛈️',  '🌩️',  '🌨️',  '🌫️',  '☁️',
+    '🐽', '🌪️', '🌊', '🌬️', '☀️', '⛅', '🌈', '⚡', '❄️', '💨',
+    '🌁', '🌤️', '🌥️', '🌦️', '🌧️', '⛈️', '🌩️', '🌨️', '🌫️', '☁️',
     '🌋', '🌍', '🌎', '🌏', '🪨', '🍂', '🍃'
 ];
+
+(async () => {
+	try {
+		const data = await rest.put(Routes.applicationGuildCommands(process.env.ID, process.env.GUILDID), { body: [commandSend] });
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		console.error(error);
+	}
+})();
 
 function getAvatar (str) {
     const hashPwd = crypto.createHash('sha1').update(str + avatarToken).digest('hex');
@@ -58,31 +83,29 @@ function getAvatar (str) {
 
 client.once(Events.ClientReady, () => { console.log('Bot turned on'); });
 
-client.on(Events.MessageCreate, async (message) => {
-    if (message.author.bot) return;
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    if (interaction.commandName !== 'send') return;
+    await interaction.deferReply({ flags: 64 });
 
-    const channel = message.channel;
     const anonymousChannel = await client.channels.fetch(anonymousChannelID);
     const modChannel = await client.channels.fetch(modChannelID);
+    const theMessage = interaction.options.getString('message');
     
-    const replyAuthorUsername = message.author.username;
     let anonEmbed;
     let logEmbed;
 
-    const r = Math.floor(Math.random() * 100000);
-    const d = new Date();
-    const messageStamp = '#' + r + ' ' + d.toLocaleDateString();
+    // console.log(anonymousChannel);
 
-    if (channel.id !== inputChannelID) return;
-    if (roleName && message.member.roles.cache.some((role) => role.name === roleName)) return; // role blacklist
+    // if (roleName && interaction.user.roles.cache.some((role) => role.name === roleName)) return; // role blacklist
 
     try {
         anonEmbed = new EmbedBuilder()
             // .setColor('#117557ff')
             // .setTitle('Anonymous Message')
             
-            .setAuthor({ name: getAvatar(message.author.id) })
-            .setDescription(String(message.content))
+            .setAuthor({ name: getAvatar(interaction.user.id) })
+            .setDescription(String(theMessage))
             // .addField('Stamp', messageStamp, true)
 
             .setTimestamp()
@@ -101,8 +124,8 @@ client.on(Events.MessageCreate, async (message) => {
             // .setColor('#117557ff')
             // .setTitle('Anonymous Message')
             
-            .setAuthor({ name: replyAuthorUsername })
-            .setDescription(String(message.content))
+            .setAuthor({ name: interaction.user.username })
+            .setDescription(String(theMessage))
             // .addField('Stamp', messageStamp, true)
 
             .setTimestamp()
@@ -114,11 +137,7 @@ client.on(Events.MessageCreate, async (message) => {
         });
     }
 
-    try {
-        message.delete();
-    } catch(error){
-        console.error('Error deleting message:', error.message);
-    }
+    await interaction.editReply('Sent!');
 });
 
 client.login(token);
